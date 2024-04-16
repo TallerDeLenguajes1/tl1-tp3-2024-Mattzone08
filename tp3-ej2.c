32ac751ebbed9d541843673130f82f64f2466b79
“Matias” <mattmedi08@gmail.com>
2024-04-16 13:48:52 -0300

    puntos 2 incompleto

---
+++ tp3-ej2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+int main(){
+
+    char *V[] = (char *) malloc(5 * sizeof(char *));
+    char nombre[100];
+    char *nombreGuard;
+    
+
+
+
+   return 0; 
+}
